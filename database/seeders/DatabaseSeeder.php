fe5cc5ec27e28f37242281dd39edb97f0cc552c3
NatiEfraim <nati.efraim@gmail.com>
2026-06-26 16:35:25 +0300

    Adiding BookingSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         UserSeeder::class,
         StatusSeeder::class,
         ProductSeeder::class,
-        ProductLikeSeeder::class
-        // BookingSeeder::class,
+        ProductLikeSeeder::class,
+        BookingSeeder::class,
         ]);
         // User::factory(10)->create();
 
